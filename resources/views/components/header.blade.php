137d41a5b6e99c3675e1134c5d104dc8053d37ee
Ervan Chodry <ervanchodry112@gmail.com>
2024-04-20 14:34:23 +0700

    Initial Commit

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,11 @@
+<header id="header" class="header bg-dark fixed-top d-flex align-items-center">
+
+    <div class="d-flex align-items-center justify-content-between">
+        <a href="index.html" class="logo d-flex align-items-center">
+            <span class="d-none text-secondary d-lg-block">CV. Karya Mandiri</span>
+        </a>
+        <i class="bi bi-list toggle-sidebar-btn text-secondary"></i>
+    </div><!-- End Logo -->
+
+
+</header><!-- End Header -->
---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,55 @@
+<aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+        <li class="nav-item">
+            <a class="nav-link " href="#">
+                <i class="bi bi-house-door"></i>
+                <span>Home</span>
+            </a>
+        </li><!-- End Dashboard Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#master-data" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-database"></i><span>Master Data</span><i class="bi bi-chevron-down ms-auto"></i>
+            </a>
+            <ul id="master-data" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                <li>
+                    <a href="#">
+                        <i class="bi bi-circle"></i><span>Produk</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="bi bi-circle"></i><span>Users</span>
+                    </a>
+                </li>
+            </ul>
+        </li><!-- End Components Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#">
+                <i class="bi bi-cart-fill"></i>
+                <span>Pembayaran</span>
+            </a>
+        </li><!-- End Profile Page Nav -->
+
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#">
+                <i class="bi bi-table"></i>
+                <span>Penjualan</span>
+            </a>
+        </li><!-- End Profile Page Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#">
+                <i class="bi bi-bank"></i>
+                <span>Profil Toko</span>
+            </a>
+        </li><!-- End Profile Page Nav -->
+
+
+    </ul>
+
+</aside>
